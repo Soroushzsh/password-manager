c409339738ccbd400e7fed21bcb3b85e2e8cb322
Soroush Shobeiri <s.zakershobeiri@yaraholding.com>
2023-09-22 17:10:18 +0330

    [TEST] implement tests for encryption flow

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -1,18 +1,38 @@
 import pytest
-from fernet_encryptor import FernetEncryptor
+from cryptography.fernet import InvalidToken, Fernet
+
+from utils.custom_encryption import FernetEncryptor
 
 
+# Test the FernetEncryptor class
 class TestFernetEncryptor:
-    @pytest.fixture
-    def encryptor(self):
-        key = b'V2KNo9nZmJXrYOXE-HFz7PVkxJcd4iwfKHqsPRrwKZw='
-        return FernetEncryptor(key)
 
-    def test_encrypt_and_decrypt(self, encryptor):
-        plaintext = "This is a secret message."
-        encrypted_data = encryptor.encrypt(plaintext)
-        decrypted_data = encryptor.decrypt(encrypted_data)
+    # Test encryption and decryption with valid key
+    def test_valid_encryption_decryption(self):
+        key = Fernet.generate_key()
+        encryptor = FernetEncryptor(key)
+        plaintext = "This is a secret message"
+        
+        # Encrypt the plaintext
+        ciphertext = encryptor.encrypt(plaintext)
+
+        # Decrypt the ciphertext and check if it matches the original plaintext
+        decrypted_data = encryptor.decrypt(ciphertext)
         assert decrypted_data == plaintext
 
-if __name__ == "__main__":
-    pytest.main()
+    # Test decryption with invalid key (should return None)
+    def test_invalid_decryption(self):
+        # Generate two different keys
+        key1 = Fernet.generate_key()
+        key2 = Fernet.generate_key()
+        
+        # Encrypt with one key and attempt decryption with another
+        encryptor1 = FernetEncryptor(key1)
+        encryptor2 = FernetEncryptor(key2)
+        plaintext = "This is a secret message"
+        
+        ciphertext = encryptor1.encrypt(plaintext)
+        
+        # Attempt decryption with the wrong key
+        decrypted_data = encryptor2.decrypt(ciphertext)
+        assert decrypted_data is None
